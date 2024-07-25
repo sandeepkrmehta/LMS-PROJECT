cfd727d2e187c335b533c491de5b305334dfdf4c
Sandeep Mehta <112331347+sandeepkrmehta@users.noreply.github.com>
2024-07-26 01:03:21 +0530

    Added Login Singup Button

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -1,10 +1,20 @@
-import { AiFillCloseCircle } from "react-icons/ai";
-import { FiMenu } from "react-icons/fi";
-import { Link } from "react-router-dom";
+import {AiFillCloseCircle} from 'react-icons/ai';
+import {FiMenu} from 'react-icons/fi';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
 
 import Footer from '../Components/Footer';
-
+// import { logout } from '../Redux/Slices/AuthSlice';
 function HomeLayout({ children }) {
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    // for checking if user is logged in
+    const isLoggedIn = useSelector((state) => state?.auth?.isLoggedIn);
+
+    // for displaying the options acc to role
+    const role = useSelector((state) => state?.auth?.role);
 
     function changeWidth() {
         const drawerSide = document.getElementsByClassName("drawer-side");
@@ -16,7 +26,15 @@ function HomeLayout({ children }) {
         element[0].checked = false;
 
         const drawerSide = document.getElementsByClassName("drawer-side");
-        drawerSide[0].style.width = 0;
+        drawerSide[0].style.width = '0';
+    }
+
+    async function handleLogout(e) {
+        e.preventDefault();
+
+        // const res = await dispatch(logout());
+        // if(res?.payload?.success)
+        navigate("/");
     }
 
     return (
@@ -25,7 +43,7 @@ function HomeLayout({ children }) {
                 <input className="drawer-toggle" id="my-drawer" type="checkbox" />
                 <div className="drawer-content">
                     <label htmlFor="my-drawer" className="cursor-pointer relative">
-                        <FiMenu
+                        <FiMenu 
                             onClick={changeWidth}
                             size={"32px"}
                             className="font-bold text-white m-4"
@@ -33,8 +51,9 @@ function HomeLayout({ children }) {
                     </label>
                 </div>
                 <div className="drawer-side w-0">
-                    <label htmlFor="my-drawer" className="drawer-overlay"></label>
-                    <ul className="menu p-4 w-48 sm:w-80 bg-base-100 text-base-content relative">
+                    <label htmlFor="my-drawer" className="drawer-overlay">
+                    </label>
+                    <ul className="menu p-4 w-48 h-[100%] sm:w-80 bg-base-200 text-base-content relative">
                         <li className="w-fit absolute right-2 z-50">
                             <button onClick={hideDrawer}>
                                 <AiFillCloseCircle size={24} />
@@ -43,20 +62,60 @@ function HomeLayout({ children }) {
                         <li>
                             <Link to="/">Home</Link>
                         </li>
+
+                        {isLoggedIn && role === 'ADMIN' && (
+                            <li>
+                                <Link to="/admin/dashboard"> Admin DashBoard</Link>
+                            </li>
+                        )}
+                        {/* {isLoggedIn && role === 'ADMIN' && (
+                            <li>
+                                <Link to="/course/create"> Create new course</Link>
+                            </li>
+                        )} */}
+
                         <li>
                             <Link to="/courses">All Courses</Link>
                         </li>
+
                         <li>
                             <Link to="/contact">Contact Us</Link>
                         </li>
+
                         <li>
                             <Link to="/about">About Us</Link>
                         </li>
+
+                        {!isLoggedIn && (
+                            <li className="absolute bottom-4 w-[90%]">
+                                <div className="w-full flex items-center justify-center">
+                                    <button className='btn-primary px-4 py-1 font-semibold rounded-md w-full'>
+                                        <Link to="/login">Login</Link>
+                                    </button>
+                                    <button className='btn-secondary px-4 py-1 font-semibold rounded-md w-full'>
+                                        <Link to="/signup">Signup</Link>
+                                    </button>
+                                </div>
+                            </li>
+                        )}
+
+                        {isLoggedIn && (
+                            <li className="absolute bottom-4 w-[90%]">
+                                <div className="w-full flex items-center justify-center">
+                                    <button className='btn-primary px-4 py-1 font-semibold rounded-md w-full'>
+                                        <Link to="/user/profile">Profile</Link>
+                                    </button>
+                                    <button className='btn-secondary px-4 py-1 font-semibold rounded-md w-full'>
+                                        <Link onClick={handleLogout}>Logout</Link>
+                                    </button>
+                                </div>
+                            </li>
+                        )}
                     </ul>
                 </div>
             </div>
 
-            {children}
+            { children }
 
             <Footer />
         </div>
