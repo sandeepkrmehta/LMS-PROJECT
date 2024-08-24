afea8fff57b6e099cd75c6724760f82cea1a893a
Sandeep Mehta <112331347+sandeepkrmehta@users.noreply.github.com>
2024-08-25 03:23:15 +0530

    Added about us page and fixed loggin signup button

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -36,56 +36,77 @@ function AboutUs() {
             />
           </div>
         </div>
-      </div>
-      <div className="carousel w-full">
-        <div id="slide1" className="carousel-item relative w-full">
-          <img src={apj} className="w-full" />
-          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-            <a href="#slide4" className="btn btn-circle">
-              ❮
-            </a>
-            <a href="#slide2" className="btn btn-circle">
-              ❯
-            </a>
-          </div>
-        </div>
-        <div id="slide2" className="carousel-item relative w-full">
-          <img src={billGates} className="w-full" />
-          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-            <a href="#slide1" className="btn btn-circle">
-              ❮
-            </a>
-            <a href="#slide3" className="btn btn-circle">
-              ❯
-            </a>
-          </div>
-        </div>
-        <div id="slide3" className="carousel-item relative w-full">
-          <img src={nelsonMandela} className="w-full" />
-          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-            <a href="#slide2" className="btn btn-circle">
-              ❮
-            </a>
-            <a href="#slide4" className="btn btn-circle">
-              ❯
-            </a>
-          </div>
-        </div>
-        <div id="slide4" className="carousel-item relative w-full">
-          <img src={steveJobs} className="w-full" />
-          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-            <a href="#slide3" className="btn btn-circle">
-              ❮</a><a href="#slide1" className="btn btn-circle">❯</a>
-          </div>
-        </div>
-        <div id="slide4" className="carousel-item relative w-full">
-          <img src={einstein} className="w-full" />
-          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-            <a href="#slide3" className="btn btn-circle">
-              ❮</a><a href="#slide1" className="btn btn-circle">❯</a>
-          </div>
-        </div>
 
+        <div className="carousel w-1/2 m-auto my-16 ">
+            <div id="slide1" className="carousel-item relative w-full">
+              <div className="flex flex-col item-center justify-center gap-4 px-[15%]">
+                <img src={nelsonMandela} className="w-40 rounded-full border-2 border-gray-400"/>
+                   <p className="text-xl text-gray-200">
+                    {"Education is the most powerful tool you can use to change the world."}                    
+                    </p>
+                   <h3 className="text-2xl font-semibold">Nelson Mandela</h3>
+                   <div className="absolute flex justify-between transform -translate-y-1/2  left-5 right-5 top-1/2">
+                      <a href="#slide5" className="btn btn-circle"></a>
+                       <a href="#slide2" className="btn btn-circle"></a>
+                    </div>
+                </div>
+            </div>
+            <div id="slide2" className="carousel-item relative w-full">
+              <div className="flex flex-col item-center justify-center gap-4 px-[15%]">
+                <img src={apj} className="w-40 rounded-full border-2 border-gray-400"/>
+                  <p className="text-xl text-gray-200">
+                    {"If you want to shine like the sun, first burn like the sun."}                    
+                  </p>
+                   <h3 className="text-2xl font-semibold">Dr. APJ Abdul kalam</h3>
+
+                  <div className="absolute flex justify-between transform -translate-y-1/2  left-5 right-5 top-1/2">
+                    <a href="#slide1" className="btn btn-circle"></a>
+                    <a href="#slide3" className="btn btn-circle"></a>
+                  </div>
+              </div>
+            </div>
+            <div id="slide3" className="carousel-item relative w-full">
+              <div className="flex flex-col item-center justify-center gap-4 px-[15%]">
+                <img src={einstein} className="w-40 rounded-full border-2 border-gray-400"/>
+                  <p className="text-xl text-gray-200">
+                    {"Life is like riding a bicycle. To keep your balance, you must keep moving."}                    
+                  </p>
+                  <h3 className="text-2xl font-semibold">Albert Einstein</h3>
+                  <div className="absolute flex justify-between transform -translate-y-1/2  left-5 right-5 top-1/2">
+                    <a href="#slide2" className="btn btn-circle"></a>
+                     <a href="#slide4" className="btn btn-circle"></a>
+                  </div>
+              </div>
+            </div>
+            <div id="slide4" className="carousel-item relative w-full">
+              <div className="flex flex-col item-center justify-center gap-4 px-[15%]">
+                <img src={steveJobs} className="w-40 rounded-full border-2 border-gray-400"/>
+                    <p className="text-xl text-gray-200">
+                    {"Your time is limited, so don’t waste it living someone else’s life."}                    
+                    </p>
+                    <h3 className="text-2xl font-semibold">Steve Jobs</h3>
+                  <div className="absolute flex justify-between transform -translate-y-1/2  left-5 right-5 top-1/2">
+                     <a href="#slide3" className="btn btn-circle"></a>
+                     <a href="#slide5" className="btn btn-circle"></a>
+                  </div>
+              </div>
+            </div>       
+            <div id="slide5" className="carousel-item relative w-full">
+              <div className="flex flex-col item-center justify-center gap-4 px-[15%]">
+                <img src={billGates} className="w-40 rounded-full border-2 border-gray-400"/>
+                    <p className="text-xl text-gray-200">
+                    {"It’s fine to celebrate success, but it is more important to heed the lessons of failure."}                    
+                    </p>
+                    <h3 className="text-2xl font-semibold">Bill Gates</h3>
+                  <div className="absolute flex justify-between transform -translate-y-1/2  left-5 right-5 top-1/2">
+                    <a href="#slide4" className="btn btn-circle"></a>
+                    <a href="#slide1" className="btn btn-circle"></a>
+                  </div>
+              </div>
+            </div>
+          </div>
+
+        
       </div>
     </HomeLayout>
   );
